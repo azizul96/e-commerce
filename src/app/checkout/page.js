b644391ca92fca9ad8b64f33bf62f30b7917b321
azizul96 <alifalamin42@gmail.com>
2024-03-06 21:57:32 +0600

    e-shop done

--- src/app/checkout/page.js
+++ src/app/checkout/page.js
@@ -1,10 +1,11 @@
-"use client";
+"use client"
+
 import { GlobalContext } from "@/context";
 import { fetchAllAddresses } from "@/services/address";
 import { createNewOrder } from "@/services/order";
 import { callStripeSession } from "@/services/stripe";
 import { loadStripe } from "@stripe/stripe-js";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 import { PulseLoader } from "react-spinners";
 import { toast } from "react-toastify";
@@ -25,8 +26,9 @@ const Checkout = () => {
   const [orderSuccess, setOrderSuccess] = useState(false);
 
   const router = useRouter();
-  const params = useSearchParams();
+  // const params = useSearchParams();
 
+  
   const publishableKey =
     "pk_test_51OECy6AzWIKBxtk8lkHDZMIVvYP8dvsDjEyjiF6oCHotePDXOESX6SS9Xl6cmfVkz0w6qHHrt5iGZv6yd8kbSZWt00jWYW0KbM";
   const stripePromise = loadStripe(publishableKey);
@@ -49,7 +51,7 @@ const Checkout = () => {
 
       if (
         isStripe &&
-        params.get("status") === "success" &&
+        // params.get("status") === "success" &&
         cartItems &&
         cartItems.length > 0
       ) {
@@ -94,7 +96,7 @@ const Checkout = () => {
     }
 
     createFinalOrder();
-  }, [params.get("status"), cartItems]);
+  }, [cartItems]);
 
   function handleSelectedAddress(getAddress) {
     if (getAddress._id === selectedAddress) {
